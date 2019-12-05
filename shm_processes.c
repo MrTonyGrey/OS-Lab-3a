3c410014ff3be10d265524927a852f9580c451be
Anthony Gordon <38927323+MrTonyGrey@users.noreply.github.com>
2019-12-05 14:59:33 -0500

    shm_processes.c file

--- shm_processes.c
+++ shm_processes.c
@@ -8,64 +8,84 @@
 
 void  ClientProcess(int []);
 
-int  main(int  argc, char *argv[])
+int main(int argc, char *argv[])
 {
-     int    ShmID;
-     int    *ShmPTR;
-     pid_t  pid;
-     int    status;
+	int ShmID;
+	int *ShmPTR;
+	pid_t pid;
+	int status;
+	int index;
 
-     if (argc != 5) {
-          printf("Use: %s #1 #2 #3 #4\n", argv[0]);
-          exit(1);
-     }
+	if (argc != 11) {
+		printf("Use: %s #1 #2 #3 … #10\n", argv[0]);
+		exit(1);
+	}
 
-     ShmID = shmget(IPC_PRIVATE, 4*sizeof(int), IPC_CREAT | 0666);
-     if (ShmID < 0) {
-          printf("*** shmget error (server) ***\n");
-          exit(1);
-     }
-     printf("Server has received a shared memory of four integers...\n");
+	ShmID = shmget(IPC_PRIVATE, 10 * sizeof(int), IPC_CREAT | 0666);
+	if (ShmID < 0) {
+		printf("*** shmget error (server) ***\n");
+		exit(1);
+	}
+	printf("Server has received a shared memory of ten integers...\n");
 
-     ShmPTR = (int *) shmat(ShmID, NULL, 0);
-     if (*ShmPTR == -1) {
-          printf("*** shmat error (server) ***\n");
-          exit(1);
-     }
-     printf("Server has attached the shared memory...\n");
+	ShmPTR = (int *)shmat(ShmID, NULL, 0);
+	if (*ShmPTR == -1) {
+		printf("*** shmat error (server) ***\n");
+		exit(1);
+	}
+	printf("Server has attached the shared memory...\n");
 
-     ShmPTR[0] = atoi(argv[1]);
-     ShmPTR[1] = atoi(argv[2]);
-     ShmPTR[2] = atoi(argv[3]);
-     ShmPTR[3] = atoi(argv[4]);
-     printf("Server has filled %d %d %d %d in shared memory...\n",
-            ShmPTR[0], ShmPTR[1], ShmPTR[2], ShmPTR[3]);
+	for (index = 0; index < 10; index++)
+		ShmPTR[index] = atoi(argv[index + 1]);
 
-     printf("Server is about to fork a child process...\n");
-     pid = fork();
-     if (pid < 0) {
-          printf("*** fork error (server) ***\n");
-          exit(1);
-     }
-     else if (pid == 0) {
-          ClientProcess(ShmPTR);
-          exit(0);
-     }
+	printf("Server has filled ");
+	for (index = 0; index < 10; index++)
+		printf("%d ", ShmPTR[index]);
+	printf("in shared memory...\n");
 
-     wait(&status);
-     printf("Server has detected the completion of its child...\n");
-     shmdt((void *) ShmPTR);
-     printf("Server has detached its shared memory...\n");
-     shmctl(ShmID, IPC_RMID, NULL);
-     printf("Server has removed its shared memory...\n");
-     printf("Server exits...\n");
-     exit(0);
+	printf("Server is about to fork a child process...\n");
+	pid = fork();
+	if (pid < 0) {
+		printf("*** fork error (server) ***\n");
+		exit(1);
+	} else if (pid == 0) {
+		ClientProcess(ShmPTR);
+		exit(0);
+	}
+
+	wait(&status);
+	printf("Server has detected the completion of its child...\n");
+
+	printf("Server found ");
+	for (index = 0; index < 10; index++)
+		printf("%d ", ShmPTR[index]);
+	printf("in shared memory, (from child process)...\n");
+
+	shmdt((void *)ShmPTR);
+	printf("Server has detached its shared memory...\n");
+	shmctl(ShmID, IPC_RMID, NULL);
+	printf("Server has removed its shared memory...\n");
+	printf("Server exits...\n");
+	exit(0);
 }
 
 void  ClientProcess(int  SharedMem[])
 {
-     printf("   Client process started\n");
-     printf("   Client found %d %d %d %d in shared memory\n",
-                SharedMem[0], SharedMem[1], SharedMem[2], SharedMem[3]);
-     printf("   Client is about to exit\n");
+     int index;
+
+	printf("   Client process started\n");
+
+	printf("   Client found ");
+	for (index = 0; index < 10; index++)
+		printf("%d ", SharedMem[index]);
+	printf("in shared memory...\n");
+
+	printf("    Client has filled ");
+	for (index = 0; index < 10; index++) {
+		SharedMem[index] = index;
+		printf("%d ", SharedMem[index]);
+	}
+	printf("in shared memory...\n");
+
+	printf("   Client is about to exit\n");
 }
